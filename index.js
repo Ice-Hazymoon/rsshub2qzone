e9a254e10baa688901d596d7aa98a046c31727f4
Hazymoon <imiku.me@gmail.com>
2019-01-10 01:07:49 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,136 @@
+"use strict";
+const credentials = require('./credentials');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const Parser = require('rss-parser');
+const Agent = require('socks5-https-client/lib/Agent');
+const dayjs = require('dayjs');
+const fs = require('fs');
+const fileType = require('file-type');
+const path = require('path');
+const del = require('del');
+
+const sendShuoshuo = require('./sendShuoshuo');
+
+const log = (log) => {
+    let date = dayjs(new Date()).format('YY年M月D日HH:mm:ss');
+    console.log(`${date}: \n${log}\n`);
+}
+
+let upTime = new Object(); // 保存rss每次拉取的时间
+const baseURL = 'https://rsshub.app';
+
+function grss(config, timeout) {
+    if (!timeout) timeout = 1000 * 60 * 5;
+    if (!upTime[config.name]) timeout = 1000 * 60 * 1;
+    setTimeout(() => {
+        rp.get(baseURL + config.url, {
+            timeout: 1000 * 60,
+            qs: {
+                limit: 1
+            }
+        })
+            .then(async e => {
+                // 解析RSS
+                const parser = new Parser();
+                let feed = await parser.parseString(e);
+
+                const date_published = dayjs(feed.items[0].pubDate).unix();
+                if (!upTime[config.name]) { // 如果不存在说明是第一次请求
+                    log('首次请求' + config.name);
+                    upTime[config.name] = date_published;
+                    grss(config);
+                    return false;
+                }
+
+                if (upTime[config.name] < date_published) { //有更新
+                    log('发现更新' + config.name)
+
+                    if (feed.items[0].title.search('Re') !== -1) { // 如果是回复类型的推文则不推送
+                        log('回复推文，不推送');
+                        grss(config);
+                        return false;
+                    }
+
+                    // 解析HTML
+                    const $ = cheerio.load(feed.items[0].content);
+
+                    let imgArr = new Array();
+
+                    if ($('img').length > 0){ // 如果有图片，请求并转换为base64编码
+                        let promises = new Array();
+                        $('img').each(function () {
+                            let src = $(this).attr('src');
+
+                            // 把http链接转换成https
+                            if(/https?/.test(src)){
+                                src = src.replace(/https?/, 'https');
+                            }
+                            
+                            promises.push(rp({
+                                method: 'GET',
+                                url: src,
+                                timeout: 1000 * 60,
+                                agentClass: Agent,
+                                agentOptions: {
+                                    socksHost: '127.0.0.1',
+                                    socksPort: 1080
+                                },
+                                encoding: null
+                            }))
+                        })
+                        try {
+                            let images = await Promise.all(promises);
+                            images.forEach(response => {
+                                const imgType = fileType(response).ext;
+                                const imgPath = path.relative(process.cwd(), __dirname + `/tmp/${dayjs().valueOf()}${~~(Math.random() * 10000)}.${imgType}`);
+                                fs.writeFileSync(imgPath, response);
+                                imgArr.push(imgPath);
+                            });
+                        } catch (error) {
+                            log(config.name + '：图片抓取失败' + error);
+                            grss(config, 1000 * 60 * 1);
+                            return false;
+                        }
+                    }
+                    const message = {
+                        text: `${config.name}更新推送`,
+                        content: feed.items[0].contentSnippet,
+                        url: feed.items[0].link,
+                        date: dayjs(feed.items[0].pubDate).format('YY年M月D日HH:mm:ss')
+                    }
+
+                    const msg = 
+                        `${message.text}\n` + 
+                        `内容：${message.content}\n` + 
+                        `原链接：${message.url}\n` + 
+                        `日期：${message.date}`
+
+                    sendShuoshuo(msg, imgArr)
+                        .then(() => {
+                            log(config.name + '更新发送成功');
+                            upTime[config.name] = date_published;
+                            grss(config);
+                            del.sync(imgArr);
+                        }).catch(error => {
+                            log(config.name + ' 更新发送失败：' + error);
+                            grss(config, 1000 * 60 * 1);
+                            del.sync(imgArr);
+                        })
+                } else { //没有更新
+                    log(config.name + ' 没有更新  最后更新于：' + dayjs(feed.items[0].pubDate).format('YY年M月D日HH:mm:ss'));
+                    grss(config);
+                }
+            })
+            .catch(error => {
+                log(config.name + '请求RSSHub失败' + error);
+                grss(config, 1000 * 60 * 1);
+            })
+    }, timeout);
+};
+
+credentials.urls.forEach((config, index) => {
+    setTimeout(() => {
+        grss(config)
+    }, 1000 * 10 * index);
+})
---
+++ test.js
@@ -0,0 +1,114 @@
+const puppeteer = require('puppeteer');
+const credentials = require('./credentials');
+const shuoshuo = '测试说说';
+const photos = ['./tmp/1.jpg', './tmp/2.jpg', './tmp/3.jpg'];
+
+(async () => {
+    const timeout = function (delay) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                try {
+                    resolve(1)
+                } catch (e) {
+                    reject(0)
+                }
+            }, delay);
+        })
+    }
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // 设置宽高
+    await page.setViewport({
+        height: 736,
+        width: 414
+    });
+
+    // 设置UA
+    await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1');
+
+    // 设置拦截器，在无头浏览器内运行时不加载图片和视频资源
+    await page.setRequestInterception(true);
+    page.on('request', interceptedRequest => {
+        if (interceptedRequest.resourceType() === 'image' || interceptedRequest.resourceType() === 'media') {
+            interceptedRequest.respond({
+                status: 200,
+                contentType: 'image/gif',
+                body: Buffer.from('R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=', 'base64')
+            })
+        } else {
+            interceptedRequest.continue();
+        }
+    });
+
+    await page.goto('https://i.qq.com');
+
+    try {
+        await page.type('#u', credentials.qq_number, {
+            delay: 100
+        });
+        await page.type('#p', credentials.password, {
+            delay: 100
+        });
+
+        await page.click('#go');
+
+        await timeout(5000);
+
+        let loginStatus = await page.content();
+        if (loginStatus.indexOf(credentials.username) != -1) {
+            console.log('登陆成功');
+        } else {
+            console.log('登陆失败');
+            return false;
+        }
+        await page.screenshot({path: './登陆成功.jpg'})
+
+        await page.click('#page-content .tweet-txt button');
+
+        await timeout(500);
+
+        await page.type('.write-text.J_textareaWrapper textarea', shuoshuo);
+
+        // 如果有图片
+        if (photos.length > 0) {
+            // 上传高清图片
+            await page.click('.ui-switch');
+            const input = await page.$('#addphoto');
+
+            // 最多只能上传9张照片
+            if (photos.length > 9) {
+                photos.splice(0, 9)
+            }
+            await input.uploadFile(...photos);
+        }
+
+        await page.click('#form-0-submit');
+
+        let time = 0;
+        let verifySS = setInterval(async () => {
+            let html = await page.content();
+            if(html.indexOf('说说发表成功') !== -1){
+                clearInterval(verifySS);
+                await browser.close();
+                resolve();
+            }else{
+                if(time>15){
+                    clearInterval(verifySS);
+                    await browser.close();
+                    reject('说说发送超时');
+                    return false;
+                }else{
+                    time++;
+                }
+            }
+        }, 1000)
+
+        await page.screenshot({path: './发送成功.jpg'})
+        await browser.close();
+    } catch (error) {
+        console.log(error)
+        await browser.close();
+    }
+})()
